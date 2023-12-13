6178c233a87d171ec76204e55b07dc29912c56bc
Jassi Bacha <klikster@gmail.com>
2023-12-12 17:54:01 -0800

    Restructuring src and fixed TodoList.test.js

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from './TodoList';
 
 it('renders without crashing', function () {
@@ -12,42 +12,40 @@ it('matches snapshot', function () {
 });
 
 it('can add a new todo', function () {
-    const { getByPlaceholderText, queryByText } = render(<TodoList />);
+    render(<TodoList />);
 
     // check there's no Delete button
-    expect(queryByText('X')).not.toBeInTheDocument();
+    expect(screen.queryByText('X')).not.toBeInTheDocument();
 
-    const taskInput = getByPlaceholderText('Enter a new task');
-    const submitBtn = queryByText('Add');
+    const taskInput = screen.getByPlaceholderText('Enter a new task');
+    const submitBtn = screen.getByText('Add');
 
     // fill out the form
     fireEvent.change(taskInput, { target: { value: 'A Task' } });
     fireEvent.click(submitBtn);
 
     // todo exists
-    const deleteBtn = queryByText('X');
+    const deleteBtn = screen.queryByText('X');
     expect(deleteBtn).toBeInTheDocument();
 });
 
 it('can delete a todo', function () {
-    const { getByPlaceholderText, getByText, queryByText } = render(
-        <TodoList />
-    );
+    render(<TodoList />);
 
-    const taskInput = getByPlaceholderText('Enter a new task');
-    const submitBtn = getByText('Add');
+    const taskInput = screen.getByPlaceholderText('Enter a new task');
+    const submitBtn = screen.getByText('Add');
 
     // fill out the form
     fireEvent.change(taskInput, { target: { value: 'A Task' } });
     fireEvent.click(submitBtn);
 
     // todo exists
-    const deleteBtn = getByText('X');
+    const deleteBtn = screen.getByText('X');
     expect(deleteBtn).toBeInTheDocument();
 
     // delete the todo
     fireEvent.click(deleteBtn);
 
     // todo is gone!
-    expect(queryByText('X')).not.toBeInTheDocument();
+    expect(screen.queryByText('X')).not.toBeInTheDocument();
 });
